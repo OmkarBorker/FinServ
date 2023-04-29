f05b0db5e85ae966b661906b6c4727cda2e766c2
Omkar Borker <omkarborker15@gmail.com>
2023-04-29 16:55:46 +0530

    modified:   server/routes/kpi.js

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -6,10 +6,9 @@ const router = express.Router()
 router.get("/kpis", async(req, res) => {
     try{
         const kpis = await KPI.find();
-        res.json({ message: "hello world" });
         res.status(200).json(kpis);
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        res.sendStatus(404).json({ message: error.message });
     }
 });
 
